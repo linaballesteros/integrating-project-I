27a9b9630ca9fffc29e3577ff0306aa96d347d69
Davidgp04 <104110970+Davidgp04@users.noreply.github.com>
2023-09-10 23:20:53 -0500

    added claim request form

--- seeku/app/forms.py
+++ seeku/app/forms.py
@@ -5,3 +5,14 @@ class BlockFilterForm(forms.Form):
         required=False,
         widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
     )
+class ClaimObject(forms.ModelForm):
+    class Meta:
+        model=Object
+        fields=['place_found','date_found','brands','color']
+        labels={'place_found':'Dinos dónde perdiste tu objecto',
+                'date_found':'Dinos qué día perdiste tu objeto',
+                'brands':'Qué marca es tu objeto',
+                'color':'De qué color es tu objeto'}
+        widgets = {
+            'date_found': forms.DateInput(attrs={'type': 'date'})
+        }
